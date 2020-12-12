19aa3e4618de3eb18f6bd50c5c474cc2881fc42a
Chemputer <46601333+chemputer@users.noreply.github.com>
2020-12-12 01:00:50 -0500

    Initial Commit, Added .gitignore as well as the scripts

---
+++ upload_all.bat
@@ -0,0 +1,3 @@
+REM TODO: EVERYTHING:
+REM Iterate over all files in a given folder (via argument), and then pass them to the python
+REM script with the filename and title, with the title being the filename - ext + AOD_B Clan Brawl
