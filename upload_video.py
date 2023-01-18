3defa3eeef2fc3960b87959d00169a1162b3de4d
Chemputer <46601333+chemputer@users.noreply.github.com>
2023-01-18 09:53:06 -0500

    start of branch with GUI functionality

--- upload_video.py
+++ upload_video.py
@@ -21,6 +21,12 @@ import random
 import sys
 import time
 
+import tkinter as tk
+
+
+from tkinter import filedialog
+from tkinter import *
+from dotenv import load_dotenv
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from apiclient.http import MediaFileUpload
@@ -28,7 +34,7 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 
-
+load_dotenv()
 # Explicitly tell the underlying HTTP transport library not to retry, since
 # we are handling retry logic ourselves.
 httplib2.RETRIES = 1
@@ -56,8 +62,8 @@ RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 #   https://developers.google.com/youtube/v3/guides/authentication
 # For more information about the client_secrets.json file format, see:
 #   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-CLIENT_SECRETS_FILE = "C:\\Users\\bdt19\Documents_New\\Scripts\\YouTube Upload API\\client_secrets.json"
-
+#CLIENT_SECRETS_FILE = "C:\\Users\\bdt19\Documents_New\\Scripts\\YouTube Upload API\\client_secrets.json"
+CLIENT_SECRETS_FILE = os.getenv('CLIENT_SECRET_FILE')
 # This OAuth 2.0 access scope allows an application to upload files to the
 # authenticated user's YouTube channel, but doesn't allow other types of access.
 YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
@@ -172,12 +178,11 @@ def resumable_upload(insert_request):
       time.sleep(sleep_seconds)
 
 if __name__ == '__main__':
-  argparser.add_argument("--file", required=True, help="Video file to upload")
-  argparser.add_argument("--title", help="Video title", default="Test Title")
+  argparser.add_argument("--file", default="" help="Video file to upload")
+  argparser.add_argument("--title", default="" help="Video title")
 
   # changed default video description to just be blank.
-  argparser.add_argument("--description", help="Video description",
-    default="")
+  argparser.add_argument("--description", default ="" help="Video description")
 
   # changed default category to 20, gaming.
   argparser.add_argument("--category", default="20",
@@ -190,6 +195,17 @@ if __name__ == '__main__':
   argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
     default=VALID_PRIVACY_STATUSES[2], help="Video privacy status.") 
   args = argparser.parse_args()
+
+  if args.file == None or args.file is "":
+      root = tk.Tk()
+      root.withdraw()
+      args.file = filedialog.askopenfilename()
+
+  if args.title is "" or args.description is "" or args.description == None or args.title == None:
+      root = tk.Tk()
+      root.withdraw()
+      args.title = tk.simpledialog.askstring("Video Title", "Please enter a title for the video:")
+      args.description = tk.simpledialog.askstring("Video Description", "Please enter a description for the video:")
 
   if not os.path.exists(args.file):
     exit("Please specify a valid file using the --file= parameter.")
